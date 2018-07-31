7a6cb443d88ece6e39c4bca15ee8a7e78520caa1
Nina <ninaokere123@gmail.com>
2018-07-31 15:47:29 -0400

    histogram graph

--- twitter_analysis.py
+++ twitter_analysis.py
@@ -1,6 +1,13 @@
+polarity = []
+subjectivity = []
+
+
 import json
 from textblob import TextBlob
+import numpy as np
+import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
+
 
 #Get the JSON data
 tweetFile = open("./tweets.json", "r")
@@ -14,6 +21,19 @@ for tweet in tweetData:
 
     tb = TextBlob(tweet["text"])
     print(tb.polarity)
+
+    polarity.append(tb.polarity)
+    subjectivity.append(tb.subjectivity)
+
+    
+    bin_edges = [-1, -0.5, 0, 0.5, 1]
+    plt.hist(polarity, bins=bin_edges)
+    plt.xlabel ("polarity")
+    plt.ylabel ("tweets")
+    plt.show()
+
+
+
 
 
 # Textblob sample:
